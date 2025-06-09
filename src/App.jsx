d1a2d9d63a461b0acdee5703ff0de10cf4b20fd0
Mariia Skydan <kyrosika565@gmail.com>
2025-06-09 16:42:16 +0300

    add base component and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Layout } from "./components/layout/Layout";
+import HomePage from "./pages/homePage/HomePage";
+import CatalogPage from "./pages/catalogPage/CatalogPage";
+import CatalogDetailPage from "./pages/catalogDetailPage/CatalogDetailPage";
 
 function App() {
-  return <></>;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/catalog/:id" element={<CatalogDetailPage />} />
+      </Routes>
+    </Layout>
+  );
 }
 
 export default App;
---
+++ src/pages/catalogDetailPage/CatalogDetailPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import c from "./CatalogDetailPage.module.css";
+
+const CatalogDetailPage = () => {
+  return <div>CatalogDetailPage</div>;
+};
+
+export default CatalogDetailPage;
