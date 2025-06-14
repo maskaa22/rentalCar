bd329feb8e96078e5257476161828369e49b8327
Mariia Skydan <kyrosika565@gmail.com>
2025-06-14 10:58:12 +0300

    add loader for persist gate in main

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,13 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
+import Loader from "./components/loader/Loader.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
+        <PersistGate loading={<Loader />} persistor={persistor}>
         <App />
         </PersistGate>
       </Provider>
